374f234233d9ee8f3e2069225d538d109d59f1d9
Bronzehail45833 <52725641+Bronzehail45833@users.noreply.github.com>
2019-08-14 21:54:28 -0500

    Update Main.php

--- src/Wolf/Main.php
+++ src/Wolf/Main.php
@@ -35,31 +35,13 @@ class Main extends PluginBase implements Listener{
 						case 0:
 						$this->paymoneyForm($player);
 						break;
-						case 1:
-						$this->givemoneyForm($player);
-						break;
-						case 2:
-						$this->getServer()->getCommandMap()->dispatch($player, "topmoney");
-						break;
-						case 3:
-						$this->setmoneyForm($player);
-						break;
-						case 4:
-						$this->seemoneyForm($player);
-						break;
-						case 5:
-						$this->getServer()->getCommandMap()->dispatch($player, "mymoney");
+					
 					}
 				}
 			});
 			$form->setTitle("§l§eMoney §bMenu");
 			$form->setContent("§o§aPlease Choose One Command!");
-			$form->addButton("§l§6Pay Money To Another Player");
-			$form->addButton("§l§6Give Money To Another Player");
-			$form->addButton("§l§6Top Money Of Server");
-			$form->addButton("§l§6Change Money Of Another Player");
-			$form->addButton("§l§6See Money Of Another Player");
-			$form->addButton("§l§6Your Money");
+			$form->addButton("§l§6Create a Faction"
 			$form->sendToPlayer($player);
 		}
 	}
@@ -73,7 +55,7 @@ class Main extends PluginBase implements Listener{
 				if($result != null){
 					$this->playerName = $data[0];
 					$this->moneyPay = $data[1];
-					$this->getServer()->getCommandMap()->dispatch($player, "pay " . $this->playerName . $this->moneyPay);
+					$this->getServer()->getCommandMap()->dispatch($player, "f create " . $this->playerName . $this->moneyPay);
 				}
 			});
 			$form->setTitle("§l§bPay §eMoney §b To Another Player");
@@ -82,55 +64,4 @@ class Main extends PluginBase implements Listener{
 		}
 	}
 	
-	public function givemoneyForm($player){
-		if($player instanceof Player){
-			$formapi = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
-			$form = $formapi->createCustomForm(function (Player $event, array $data){
-				$player = $event->getPlayer();
-				$result = $data[0];
-				if($result != null){
-					$this->playerNamee = $data[0];
-					$this->moneyGive = $data[1];
-					$this->getServer()->getCommandMap()->dispatch($player, "givemoney " . $this->playerNamee . $this->moneyGive);
-				}
-			});
-			$form->setTitle("§l§bGive §eMoney §b To Another Player");
-			$form->addInput("§oType Player Name And Money To Give");
-			$form->sendToPlayer($player);
-		}
-	}
-	public function setmoneyForm($player){
-		if($player instanceof Player){
-			$formapi = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
-			$form = $formapi->createCustomForm(function (Player $event, array $data){
-				$player = $event->getPlayer();
-				$result = $data[0];
-				if($result != null){
-					$this->playerNamea = $data[0];
-					$this->moneyChange = $data[1];
-					$this->getServer()->getCommandMap()->dispatch($player, "setmoney " . $this->playerNamea . $this->moneyChange);
-				}
-			});
-			$form->setTitle("§l§bChange §eMoney §b Of Another Player");
-			$form->addInput("§oType Player Name And Money To Change");
-			$form->sendToPlayer($player);
-		}
-	}
 	
-	public function seemoneyForm($player){
-		if($player instanceof Player){
-			$formapi = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
-			$form = $formapi->createCustomForm(function (Player $event, array $data){
-				$player = $event->getPlayer();
-				$result = $data[0];
-				if($result != null){
-					$this->playerNamer = $data[0];
-					$this->getServer()->getCommandMap()->dispatch($player, "seemoney " . $this->playerNamer);
-				}
-			});
-			$form->setTitle("§l§bSee §eMoney §bOf Another Player");
-			$form->addInput("§oType Player Name ");
-			$form->sendToPlayer($player);
-		}
-	}
-}
